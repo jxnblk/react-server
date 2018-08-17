44defafc60ad8836ef22b9e7c9da4c17f580c2bf
Brent Jackson <jxnblk@gmail.com>
2018-08-17 09:59:22 -0400

    Add styled-components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,16 @@
 import React from 'react'
+import styled from 'styled-components'
+
+// for demonstration only
+const Root = styled.div([], {
+  fontFamily: 'system-ui, sans-serif',
+  maxWidth: '1024px',
+  padding: '32px',
+  margin: 'auto',
+  color: '#222'
+})
 
 export default () =>
-  <h1>hello</h1>
+  <Root>
+    <h1>hello</h1>
+  </Root>
--- src/server.js
+++ src/server.js
@@ -1,20 +1,31 @@
 import React from 'react'
 import { renderToString } from 'react-dom/server'
+import { ServerStyleSheet } from 'styled-components'
 import express from 'express'
 import App from './App'
 
 const assets = require('../webpack-assets.json')
 
 const app = (req, res) => {
+  const sheet = new ServerStyleSheet()
   const body = renderToString(
-    <App />
+    sheet.collectStyles(
+      <App />
+    )
   )
 
+  const styles = sheet.getStyleTags()
+
   const html = `<!DOCTYPE html>
+<html>
+<head>
 <title>hello</title>
+${styles}
+</head>
+<body>
 <div id=root>${body}</div>
 <script src='${assets.main.js}'></script>
-  `
+</body>`
 
   res.send(html)
 }
